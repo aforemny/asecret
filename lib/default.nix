ca33c32789716efb0fc77aa74fedea4c3302ea84
Alexander Foremny <aforemny@posteo.de>
2025-02-10 10:28:16 +0100

    make `sshKeyPair` configurable

--- lib/default.nix
+++ lib/default.nix
@@ -1,6 +1,10 @@
+{ lib, ... }:
 let generate = args: builtins.fromJSON (builtins.extraBuiltins.asecret ([ "generate" ] ++ args)); in
 {
   hashedPassword = secretPath: generate [ "hashed-password" secretPath ];
-  sshKeyPair = secretPath: generate [ "ssh-key-pair" "--type" "ed25519" secretPath ];
+  sshKeyPair = secretPath: lib.makeOverridable
+    ({ type }:
+      generate [ "ssh-key-pair" "--type" type secretPath ])
+    { type = "ed25519"; };
   sslCertificate = caPath: certPath: domains: generate ([ "ssl-certificate" caPath certPath ] ++ domains);
 }
