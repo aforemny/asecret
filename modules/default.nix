d56990e50ec64be7912b0e31516514c0d34a3b17
Alexander Foremny <aforemny@posteo.de>
2025-04-23 10:15:31 +0200

    do not set Nix options globally
    This is more hassle than it is worth: it is sufficient to set Nix
    options in the shell doing the system evaluations, but specifying the
    options globally means every other Nix version cannot run, of which
    there are many.

--- modules/default.nix
+++ modules/default.nix
@@ -1,9 +1,5 @@
 { pkgs, lib, ... }:
 {
-  nix.extraOptions = ''
-    plugin-files = ${pkgs.nix-plugins}/lib/nix/plugins
-    extra-builtins-file = ${../extra-builtins.nix}
-  '';
   nixpkgs.overlays = [
     (import ../pkgs)
     (_: super: { asecret-lib = import ../lib { inherit lib; }; })
